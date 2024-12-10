c028818af491d34e52dafd9f5426a17806ba1402
Barry McAuley <barrymcauley@pm.me>
2024-12-10 16:22:41 +0000

    day 9: part 2

--- src/main/java/org/example/DayNine.java
+++ src/main/java/org/example/DayNine.java
@@ -14,31 +14,55 @@ public class DayNine {
         System.out.println("Day Nine");
         var dayNine = new DayNine();
         dayNine.partOne();
+        dayNine.partTwo();
     }
 
     public void partOne() {
         System.out.println("Part One");
         try {
-            var data = Arrays.stream(Files.readString(Path.of("src/main/resources/daynine.txt")).split(""))
-                    .map(Long::parseLong)
-                    .collect(Collectors.toList());
-
+            var data = readInputFile();
             var input = calculateFileData(data);
             var listToCalc = swapSpace(input, 0, input.size() - 1).stream()
                     .filter(c -> c != -1L)
                     .toList();
-
-            var output = 0L;
-            var multiplier = 0L;
-            for (Long l : listToCalc) {
-                output += (l * multiplier);
-                multiplier += 1;
-            }
+            var output = calculateChecksum(listToCalc);
 
             System.out.println("Output: " + output);
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public void partTwo() {
+        System.out.println("Part Two");
+        try {
+            var data = readInputFile();
+            var input = calculateFileData(data);
+            var listToCalc = optimisedSwapSpace(input, input.size());
+            var output = calculateChecksum(listToCalc);
+
+            System.out.println("Output: " + output);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static long calculateChecksum(List<Long> listToCalc) {
+        var output = 0L;
+        var multiplier = 0L;
+        for (Long l : listToCalc) {
+            if (l != -1L) {
+                output += (l * multiplier);
+            }
+            multiplier += 1;
+        }
+        return output;
+    }
+
+    private static List<Long> readInputFile() throws IOException {
+        return Arrays.stream(Files.readString(Path.of("src/main/resources/daynine.txt")).split(""))
+                .map(Long::parseLong)
+                .collect(Collectors.toList());
     }
 
     private static List<Long> swapSpace(List<Long> data, int index1, int index2) {
@@ -59,6 +83,63 @@ public class DayNine {
         }
 
         return output;
+    }
+
+    private static List<Long> optimisedSwapSpace(List<Long> data, int end) {
+        var output = new ArrayList<>(data);
+
+        while (end > 0) {
+            while (end > 0 && output.get(end - 1) == -1L) {
+                end--;
+            }
+            if (end == 0) {
+                break;
+            }
+
+            var fileID = output.get(end - 1);
+            var start = end - 1;
+            while (start > 0 && output.get(start - 1).equals(fileID)) {
+                start--;
+            }
+            var idLen = end - start;
+
+            var freeSpace = findFreeSpace(output, idLen, start);
+            if (freeSpace != -1) {
+                for (int i = 0; i < idLen; i++) {
+                    var temp = output.get(freeSpace + i);
+                    output.set(freeSpace + i, output.get(start + i));
+                    output.set(start + i, temp);
+                }
+            }
+
+            end = start;
+        }
+
+        return output;
+    }
+
+    private static int findFreeSpace(List<Long> data, int len, int max) {
+        int consecutiveSpaces = 0;
+        int startPosition = -1;
+
+        for (int i = 0; i < max; i++) {
+            if (data.get(i) == -1L) {
+                if (consecutiveSpaces == -1) {
+                    startPosition = i;
+                    consecutiveSpaces = 1;
+                } else {
+                    consecutiveSpaces++;
+                }
+
+                if (consecutiveSpaces >= len) {
+                    return startPosition;
+                }
+            } else {
+                consecutiveSpaces = -1;
+            }
+        }
+
+        return -1;
     }
 
     private static List<Long> calculateFileData(List<Long> data) {
